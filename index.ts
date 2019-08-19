f0c318a34a952e0091bf6fbfb41e6142240d90cc
Marco Nisi <marco.nisi.100@gmail.com>
2019-08-19 22:57:00 +0200

    Initial release

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+interface IUseMyPosition {
+  initialPosition: Position,
+  enableHighAccuracy: boolean,
+  immediate: boolean
+}
+
+export default (props: IUseMyPosition) => {
+  const [myPosition, setMyPosition]: [Position, Function] = useState(props.initialPosition)
+
+  useEffect(() => {
+    const watcher = navigator.geolocation.watchPosition(
+      pos => setMyPosition(pos),
+      undefined,
+      { enableHighAccuracy: props.enableHighAccuracy }
+    )
+    if (props.immediate) {
+      navigator.geolocation.getCurrentPosition(
+        pos => setMyPosition(pos),
+        undefined,
+        { enableHighAccuracy: props.enableHighAccuracy }
+      )
+    }
+    return () => navigator.geolocation.clearWatch(watcher)
+  }, [])
+  return myPosition
+}
